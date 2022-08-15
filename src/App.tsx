618319fe878330f817141ea8a48a0082fdf2cb35
Damir <nuriakhmetov.d.t1998@gmail.com>
2022-08-15 09:33:28 +0300

    second commit +mui -tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import {Button} from "@mui/material";
 
 function App() {
   return (
-    <div className={'text-3xl'}>
-<p>sadfsdds</p>
+    <div>
+      <Button>show</Button>
     </div>
   );
 }
