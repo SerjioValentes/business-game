b92e824f89fdc7b1cf54fe697efeab6005317974
Damir <nuriakhmetov.d.t1998@gmail.com>
2022-08-16 17:32:07 +0300

    add swiper

--- src/components/pages/incomingPay/index.tsx
+++ src/components/pages/incomingPay/index.tsx
@@ -1,10 +1,72 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Divider, TextField, Typography} from "@mui/material";
 import Card from "@mui/material/Card";
-
+import {Swiper, SwiperSlide} from 'swiper/react';
+import 'swiper/css';
+import { FreeMode, Navigation, Thumbs } from "swiper";
 const IncomingPayments = () => {
+
+
+    const [thumbsSwiper, setThumbsSwiper] = useState<File | null>(null);
+
     return (
         <div>
+            {/*<button onClick={() => slideTo(1)} className="prepend-slide">*/}
+            {/*    Slide 1*/}
+            {/*</button>*/}
+
+
+            <Swiper
+                loop={true}
+                navigation={true}
+                // thumbs={{ swiper: thumbsSwiper }}
+                modules={[FreeMode, Navigation, Thumbs]}
+                spaceBetween={50}
+                slidesPerView={1}
+                onSlideChange={() => console.log('slide change')}
+                // onSwiper={(swiper) => console.log(swiper)}
+            >
+                <SwiperSlide>
+                    <div><TextField label={'Выручка'}/></div>
+                    <div><TextField label={'Денег на р/с'}/></div>
+                    <div><TextField label={'Коэф-т рентабильности'}/></div>
+                    <div><TextField label={'Прибыль'}/></div>
+
+                    <Divider>Расходы</Divider>
+
+                    <div><TextField label={'Расходы'}/></div>
+                    <div><TextField label={'Постоянные'}/></div>
+                    <div><TextField label={'Переменные'}/></div>
+                    <div><TextField label={'Налоги'}/></div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div><TextField label={'Выручка'}/></div>
+                    <div><TextField label={'Денег на р/с'}/></div>
+                    <div><TextField label={'Коэф-т рентабильности'}/></div>
+                    <div><TextField label={'Прибыль'}/></div>
+
+                    <Divider>Расходы</Divider>
+
+                    <div><TextField label={'Расходы'}/></div>
+                    <div><TextField label={'Постоянные'}/></div>
+                    <div><TextField label={'Переменные'}/></div>
+                    <div><TextField label={'Налоги'}/></div>
+                </SwiperSlide>
+            </Swiper>
+            {/*<Swiper*/}
+            {/*    onSwiper={setThumbsSwiper}*/}
+            {/*    loop={true}*/}
+            {/*    spaceBetween={10}*/}
+            {/*    slidesPerView={4}*/}
+            {/*    freeMode={true}*/}
+            {/*    watchSlidesProgress={true}*/}
+            {/*    modules={[FreeMode, Navigation, Thumbs]}*/}
+            {/*>*/}
+            {/*    <SwiperSlide>*/}
+            {/*        <button>show</button>*/}
+            {/*    </SwiperSlide>*/}
+            {/*</Swiper>*/}
+
             {/*<Card>*/}
             {/*    <Typography>Личный капитал</Typography>*/}
             {/*    <Typography>5000</Typography>*/}
@@ -29,58 +91,6 @@ const IncomingPayments = () => {
             {/*    <Typography>Расходы на жизнь</Typography>*/}
             {/*    <Typography>5000</Typography>*/}
             {/*</Card>*/}
-
-
-            <div>
-                <TextField
-                    label={'Выручка'}
-                />
-            </div>
-
-            <div>
-
-                <TextField
-                    label={'Денег на р/с'}
-                />
-            </div>
-            <div>
-                <TextField
-                    label={'Коэф-т рентабильности'}
-                />
-            </div>
-            <div>
-                <TextField
-                    label={'Прибыль'}
-                />
-            </div>
-
-            <Divider>Расходы</Divider>
-            <div>
-
-
-            <TextField
-                label={'Расходы'}
-            />
-            </div>
-<div>
-
-            <TextField
-                label={'Постоянные'}
-            />
-</div>
-            <div>
-
-
-            <TextField
-                label={'Переменные'}
-            />
-            </div>
-            <div>
-
-            <TextField
-                label={'Налоги'}
-            />
-            </div>
 
         </div>
     );
